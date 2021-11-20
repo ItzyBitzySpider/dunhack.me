eb303d4f353c8dd60983da5cb327bbbd0fddb517
Ocean <36119714+Oceankoh@users.noreply.github.com>
2021-11-20 20:15:56 +0800

    side bar and front end stuff

--- webApp/components/sidebarNavigation.tsx
+++ webApp/components/sidebarNavigation.tsx
@@ -2,15 +2,6 @@ import React from 'react';
 import Link from 'next/link';
 import { Sidenav, Nav } from 'rsuite';
 
-const NavLink = React.forwardRef((props, ref) => {
-	const { as, href, ...rest } = props;
-	return (
-		<Link href={href} as={as}>
-			<a ref={ref} {...rest} />
-		</Link>
-	);
-});
-
 const SidebarNavigation = () => (
 	<div className='sidebar'>
 		<nav>
@@ -42,35 +33,41 @@ const SidebarNavigation = () => (
 		</nav>
 		<style jsx>{`
 			.sidebar {
-				background-color: #2c5784;
-				width: 250px;
+				background-color: #006989;
+				width: 20%;
 				height: 100vh;
-				padding: 0 0.5rem;
 				display: flex;
-				justify-content: center;
+				// justify-content: center;
 				align-items: center;
 				float: left;
 			}
 
 			.nav-menu {
+				position: relative;
 				font-size: 20px;
-				list-style-type: none;
-				margin: 0;
-				padding: 0;
-			}
-
-			li {
-				padding: 5px;
+				list-style: none;
 			}
 
 			a {
-				color: white;
+				color: #EAEBED;
+				letter-spacing: 0.1em;
+				margin-top: 20px;
+				font-size: 1em;
+				display: block;
+				transition: all ease-out 300ms;
+				text-decoration: none;
 			}
 
 			a:hover {
-				text-decoration: none;
+				color: white;
 				font-weight: bold;
 			}
+
+			a:active{
+				color: white;
+				font-weight: bold;
+			}
+
 		`}</style>
 	</div>
 );

48253f1f11125734fd9c3fa14064eb3728c36f60
Ocean <36119714+Oceankoh@users.noreply.github.com>
2021-11-21 00:07:00 +0800

    login button no longer rendered once authenticated

--- webApp/components/sidebarNavigation.tsx
+++ webApp/components/sidebarNavigation.tsx
@@ -1,75 +1,82 @@
 import React from 'react';
 import Link from 'next/link';
-import { Sidenav, Nav } from 'rsuite';
+import { useSession } from 'next-auth/react';
 
-const SidebarNavigation = () => (
-	<div className='sidebar'>
-		<nav>
-			<ul className='nav-menu'>
-				<li>
-					<Link href='/'>
-						<a>Home</a>
-					</Link>
-				</li>
+export default function SidebarNavigation() {
+	const { data: session, status } = useSession();
+	return (
+		<>
+			<div className='sidebar'>
+				<nav>
+					<ul className='nav-menu'>
+						<li>
+							<Link href='/'>
+								<a>Home</a>
+							</Link>
+						</li>
 
-				<li>
-					<Link href='challenges'>
-						<a>Challenges</a>
-					</Link>
-				</li>
+						<li>
+							<Link href='challenges'>
+								<a>Challenges</a>
+							</Link>
+						</li>
 
-				<li>
-					<Link href='scoreboard'>
-						<a>Scoreboard</a>
-					</Link>
-				</li>
+						<li>
+							<Link href='scoreboard'>
+								<a>Scoreboard</a>
+							</Link>
+						</li>
+						{session && (
+							<Link href='profile'>
+								<a>Profile</a>
+							</Link>
+						)}
+						{!session && (
+							<li>
+								<Link href='login'>
+									<a>Login</a>
+								</Link>
+							</li>
+						)}
+					</ul>
+				</nav>
+				<style jsx>{`
+					.sidebar {
+						background-color: #14213d;
+						width: 20%;
+						height: 100vh;
+						display: flex;
+						align-items: center;
+						float: left;
+					}
 
-				<li>
-					<Link href='login'>
-						<a>Login</a>
-					</Link>
-				</li>
-			</ul>
-		</nav>
-		<style jsx>{`
-			.sidebar {
-				background-color: #006989;
-				width: 20%;
-				height: 100vh;
-				display: flex;
-				// justify-content: center;
-				align-items: center;
-				float: left;
-			}
+					.nav-menu {
+						position: relative;
+						font-size: 20px;
+						list-style: none;
+					}
 
-			.nav-menu {
-				position: relative;
-				font-size: 20px;
-				list-style: none;
-			}
+					a {
+						color: #ffda33aa;
+						letter-spacing: 0.1em;
+						margin-top: 20px;
+						font-size: 1em;
+						display: block;
+						transition: all ease-out 300ms;
+						text-decoration: none;
+					}
 
-			a {
-				color: #EAEBED;
-				letter-spacing: 0.1em;
-				margin-top: 20px;
-				font-size: 1em;
-				display: block;
-				transition: all ease-out 300ms;
-				text-decoration: none;
-			}
+					a:hover {
+						color: #ffda33;
+						font-weight: bold;
+					}
 
-			a:hover {
-				color: white;
-				font-weight: bold;
-			}
-
-			a:active{
-				color: white;
-				font-weight: bold;
-			}
-
-		`}</style>
-	</div>
-);
-
-export default SidebarNavigation;
+					a:active {
+						color: white;
+						font-weight: bold;
+					}
+				`}</style>
+			</div>
+		</>
+	);
+}
---
+++ webApp/pages/profile.tsx
@@ -0,0 +1,10 @@
+import SidebarNavigation from '../components/sidebarNavigation';
+
+export default function Profile() {
+	return (
+		<>
+			<SidebarNavigation />
+			<h1>Profiles page under construction</h1>
+		</>
+	);
+}

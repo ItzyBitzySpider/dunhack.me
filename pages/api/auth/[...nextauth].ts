e1cb983bd8cd6213a469d4e3b4f08d6783d18e6d
Sean - <sean.seah0311@gmail.com>
2021-11-21 15:20:40 +0800

    removed github login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,6 @@ import NextAuth from 'next-auth';
 import { PrismaClient } from '@prisma/client';
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import EmailProvider  from 'next-auth/providers/email';
-import GithubProvider from 'next-auth/providers/github';
 
 const prisma = new PrismaClient()
 
@@ -12,10 +11,6 @@ export default NextAuth({
 		EmailProvider({
 			server: process.env.EMAIL_SERVER,
 			from: process.env.EMAIL_FROM
-		}),
-		GithubProvider({
-			clientId: process.env.GITHUB_CLIENT_ID,
-			clientSecret: process.env.GITHUB_CLIENT_SECRET
 		})
 	],
 	pages: {

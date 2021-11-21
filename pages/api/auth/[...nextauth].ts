67eb6f95ef82a12ef93be9aa70fdbf9e4e2b023d
Ocean <36119714+Oceankoh@users.noreply.github.com>
2021-11-21 18:24:50 +0800

    custom login page

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,49 +1,49 @@
 import NextAuth from 'next-auth';
 import { PrismaClient } from '@prisma/client';
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import EmailProvider  from 'next-auth/providers/email';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import EmailProvider from 'next-auth/providers/email';
 import { signIn } from 'next-auth/react';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export default NextAuth({
 	adapter: PrismaAdapter(prisma),
 	providers: [
 		EmailProvider({
 			server: process.env.EMAIL_SERVER,
-			from: process.env.EMAIL_FROM
-		})
+			from: process.env.EMAIL_FROM,
+		}),
 	],
 	pages: {
-		// new user page here
+		signIn: 'login',
 	},
 	callbacks: {
 		async signIn({ user, account, profile, email, credentials }) {
-			let isAllowedToSignIn = false
+			let isAllowedToSignIn = false;
 			// user = {"email":"actual_email_addr"}
-			let user_email = JSON.parse(JSON.stringify(user)).email
-			if(process.env.EMAIL_WHITELIST){
+			let user_email = JSON.parse(JSON.stringify(user)).email;
+			if (process.env.EMAIL_WHITELIST) {
 				//search for user email with prisma
 				const email_whitelisted = await prisma.email_whitelist.findUnique({
 					where: {
-					  email: user_email,
+						email: user_email,
 					},
-				  })
-				if(email_whitelisted){
-					isAllowedToSignIn = true
+				});
+				if (email_whitelisted) {
+					isAllowedToSignIn = true;
 				}
-			} else if (process.env.EMAIL_REGEX){
+			} else if (process.env.EMAIL_REGEX) {
 				//regex comparison
-				const regex = process.env.EMAIL_REGEX
-				const regex_check = new RegExp(regex)
-				if(regex_check.test(user_email)){
-					isAllowedToSignIn = true
+				const regex = process.env.EMAIL_REGEX;
+				const regex_check = new RegExp(regex);
+				if (regex_check.test(user_email)) {
+					isAllowedToSignIn = true;
 				}
 			} else {
 				//email whitelist disabled, allow all users to register and login
-				isAllowedToSignIn = true
+				isAllowedToSignIn = true;
 			}
-			return isAllowedToSignIn
-		}	
-	}
-}); 
+			return isAllowedToSignIn;
+		},
+	},
+});
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,39 @@
-import Form from 'react-bootstrap/Form';
-import SidebarNavigation from '../components/sidebarNavigation';
-import {signIn, useSession} from 'next-auth/react'
+import { getCsrfToken, getProviders, signIn } from 'next-auth/react';
 
-export default function Login() {
-    const {data: session, status} = useSession();
+export default function Login({ providers, csrfToken }) {
+	console.log(providers)
 	return (
 		<>
-			{!session && <button onClick={()=>signIn()}>Sign In</button>}
+			{Object.values(providers).map((provider) => (
+				<>
+					{provider.name === 'Email' && (
+						<form method='post' action='/api/auth/signin/email'>
+							<input name='csrfToken' type='hidden' defaultValue={csrfToken} />
+							<label>
+								Email address
+								<input type='email' id='email' name='email' />
+							</label>
+							<button type='submit'>Sign in with Email</button>
+						</form>
+					)}
+					{provider.name !== 'Email' && (
+						<div key={provider.name}>
+							<button onClick={() => signIn(provider.id)}>
+								Sign in with {provider.name}
+							</button>
+						</div>
+					)}
+				</>
+			))}
 		</>
 	);
+}
+
+// Get Auth providers
+export async function getServerSideProps(context) {
+	const providers = await getProviders();
+	const csrfToken = await getCsrfToken(context);
+	return {
+		props: { providers, csrfToken },
+	};
 }

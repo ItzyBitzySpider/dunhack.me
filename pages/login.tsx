b77d73c1b2745bce74c7673b100be7e57f2a5009
Ocean <36119714+Oceankoh@users.noreply.github.com>
2021-11-24 00:59:23 +0800

    login done (i hope)

--- pages/login.tsx
+++ pages/login.tsx
@@ -37,43 +37,44 @@ export default function Login({
 		return <a>Redirecting</a>;
 	} else {
 		return (
-			<Row className='justify-content-center align-items-center h-100'>
+			<Row className='justify-content-center align-items-center h-100 g-0'>
 				<Col md={6}>
-					<h1>Sign in with</h1> 
-					<br/>
-					<Row>
-						<Form method='post' action='/api/auth/signin/email' id='test'>
-							<Row className='align-items-center justify-content-center'>
-								<Col md={10} className='h-100 g-0'>
-									<Form.Group controlId='csrfToken'>
-										<Form.Control
-											name='csrfToken'
-											type='hidden'
-											defaultValue={csrfToken}
-										/>
-									</Form.Group>
+					<h1>Sign in with</h1>
+					<br />
+					<Form method='post' action='/api/auth/signin/email' id='test'>
+						<Row className='g-0'>
+							<Col md={10} className='h-100 g-0'>
+								<Form.Group controlId='csrfToken'>
+									<Form.Control
+										name='csrfToken'
+										type='hidden'
+										defaultValue={csrfToken}
+									/>
+								</Form.Group>
 
-									<FloatingLabel label='Email address' controlId='email'>
-										<Form.Control
-											className={styles.inputField}
-											name='email'
-											type='email'
-											placeholder='Email Address'
-										/>
-									</FloatingLabel>
-								</Col>
-								<Col className='h-100'>
-									<Button className='h-100' variant='secondary' type='submit'>
-										<MdKeyboardArrowRight size='1.5rem' />
-									</Button>
-								</Col>
-							</Row>
-						</Form>
-					</Row>
+								<FloatingLabel className={styles.inputField} label='Email address' controlId='email'>
+									<Form.Control										
+										name='email'
+										type='email'
+										placeholder='Email Address'
+									/>
+								</FloatingLabel>
+							</Col>
+				
+							<Col md={2}>
+								<Button
+									className={styles.submit}
+									variant='secondary'
+									type='submit'>
+									<MdKeyboardArrowRight size='2rem' />
+								</Button>
+							</Col>
+						</Row>
+					</Form>
 					<br />
 					<Row className='justify-content-center'>
 						<span> &nbsp;&nbsp;&nbsp;Or&nbsp;&nbsp;&nbsp; </span>
-						
+
 						<style>{`
 						span{
 							display: flex;
@@ -91,12 +92,12 @@ export default function Login({
 						`}</style>
 					</Row>
 					<br />
-					<Row className='justify-content-center'>
+					<Row className='justify-content-center g-0'>
 						<Button
-							className={styles.btnGithub}
+							className={styles.btnProvider}
 							onClick={() => signIn(providers.github.id)}>
-							<GoMarkGithub size='1.3rem' className={styles.providerIcon} />
-							<GoLogoGithub size='4rem' />
+							<GoMarkGithub size='1.2rem' className={styles.providerIcon} />
+							<GoLogoGithub size='3.5rem' />
 						</Button>
 					</Row>
 				</Col>

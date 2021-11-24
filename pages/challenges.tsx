9a3224beb1706c172ee82ea083038c3631f69464
Ocean <36119714+Oceankoh@users.noreply.github.com>
2021-11-24 17:17:28 +0800

    Challenge component

--- pages/challenges.tsx
+++ pages/challenges.tsx
@@ -1,14 +1,34 @@
 import { useSession } from 'next-auth/react';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import Challenge from '../components/challenge';
 
-export default function Challenges() {
+export default function Challenges({ challenges }) {
 	const { data: session, status } = useSession();
-	if (session) {
+	const [modalShow, setModalShow] = useState(false);
+	if (!session) {
 		return (
 			<>
-				<h1>Challenges page under construction</h1>
+				<h1>Challenges</h1>
+				<Challenge chal={challenges} />
 			</>
 		);
-	}else{
+	} else {
 		return <h1>Unauthorized</h1>;
 	}
+}
+
+// Get challenges
+export async function getServerSideProps(context) {
+	const challenges = {
+		title: 'Challenge Title',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed metus neque, auctor nec mollis in, suscipit a neque. Etiam interdum est eget magna vehicula, quis.',
+		hint: 'hint',
+		files: '',
+		points: 500,
+	};
+	return {
+		props: { challenges },
+	};
 }

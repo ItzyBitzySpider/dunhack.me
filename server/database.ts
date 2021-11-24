1ecb400dc251d86bb25d4329b3e0490ebb583fbb
Sean - <sean.seah0311@gmail.com>
2021-11-24 14:44:21 +0800

    bugfix?

---
+++ server/database.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+//init DB with default values
+
+//load env config into DB
+
+//force reload of DB config to map
+
+
+//export DB config as a map for fast lookup

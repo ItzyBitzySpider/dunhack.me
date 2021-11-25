e4dfbdc3489f9d88a4af4433398e73f4a7d14776
Ocean <36119714+Oceankoh@users.noreply.github.com>
2021-11-25 16:59:39 +0800

    rough home page layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,15 @@
+import Head from 'next/head';
 import SidebarNavigation from './sidebarNavigation';
 
 export default function Layout({ children }) {
 	return (
 		<>
+			<Head>
+				<title>It'z Different CTF</title>
+				<meta charSet='utf-8' />
+				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+				<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+			</Head>
 			<div className='vh-100'>
 				<div className='row justify-content-center h-100 g-0'>
 					<div className='col-lg-2 col-md-12 col-sm-12'>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
 import Head from 'next/head';
-import SidebarNavigation from '../components/sidebarNavigation';
 import React from 'react';
 
 export default function Home() {
 	return (
 		<>
-			<h1>Home page under construction</h1>
+			<h1>Home</h1>
+			<br/><br/>
+			<h2>What is insert &lt;some name here&gt;? </h2>
+			<a>
+				It'z Different CTF is a CTF training platform. We host a collection of
+				challenges from past CTFs held in Singapore. This CTF will run
+				indefinitely.
+			</a>
+			<br/><br/><br/>
+			<h2>How does it work?</h2>
+			<a>If you are unfamiliar with CTFs, you may take a look at this </a>
+			<a href='https://www.youtube.com/watch?v=8ev9ZX9J45A'>video</a>
+			<a> and this </a>
+			<a href='https://trailofbits.github.io/ctf/'>guide</a>
+			<a> to find out more.</a>
+			<br/><br/><br/>
+			<h2>List of CTFs collated</h2>
+			<ul>
+				<li>None</li>
+			</ul><br/>
+			<h2>Sponsors</h2>
+			<a>ACSI School Funds</a>
+			<br/><br/>
 		</>
 	);
 }

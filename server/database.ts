111d0ca13eafa6abf61ab33595d2a7f06ab93a8c
Sean - <sean.seah0311@gmail.com>
2021-11-27 00:53:02 +0800

    resolved prisma too many clients issue

--- server/database.ts
+++ server/database.ts
@@ -1,6 +1,18 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient();
+let prisma
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+
+  prisma = global.prisma
+}
+
+export default prisma
 
 export async function getChallengeByCategory(categoryName) {
     try {

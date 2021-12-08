79db7d5e908f75ca56fd8d6b5e52b99cc7ea4110
Ocean <36119714+Oceankoh@users.noreply.github.com>
2021-12-09 00:49:59 +0800

    modified active sidenav for edge cases

--- components/sidebarNavigation.tsx
+++ components/sidebarNavigation.tsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import { signOut, useSession } from 'next-auth/react';
 import styles from '../styles/sidebar.module.scss';
+import { useRouter } from 'next/router';
 
 export default function SidebarNavigation() {
 	const { data: session, status } = useSession();
-	const [active, setActive] = useState(0);
+	const { asPath, pathname } = useRouter();
+	const path = asPath.split('/');
+	const [active, setActive] = useState(path[1]);
 	return (
 		<>
 			<nav className={styles.sidebar}>
@@ -13,8 +16,8 @@ export default function SidebarNavigation() {
 					<li>
 						<Link href='/'>
 							<a
-								onClick={() => setActive(0)}
-								className={active === 0 ? styles.selected : styles.text}>
+								onClick={() => setActive('')}
+								className={active === '' ? styles.selected : styles.text}>
 								Home
 							</a>
 						</Link>
@@ -23,8 +26,8 @@ export default function SidebarNavigation() {
 					<li>
 						<Link href='/challenges'>
 							<a
-								onClick={() => setActive(1)}
-								className={active === 1 ? styles.selected : styles.text}>
+								onClick={() => setActive('challenges')}
+								className={active === 'challenges' ? styles.selected : styles.text}>
 								Challenges
 							</a>
 						</Link>
@@ -33,8 +36,8 @@ export default function SidebarNavigation() {
 					<li>
 						<Link href='/scoreboard'>
 							<a
-								onClick={() => setActive(2)}
-								className={active === 2 ? styles.selected : styles.text}>
+								onClick={() => setActive('scoreboard')}
+								className={active === 'scoreboard' ? styles.selected : styles.text}>
 								Scoreboard
 							</a>
 						</Link>
@@ -43,8 +46,8 @@ export default function SidebarNavigation() {
 						<li>
 							<Link href='/profile'>
 								<a
-									onClick={() => setActive(3)}
-									className={active === 3 ? styles.selected : styles.text}>
+									onClick={() => setActive('profile')}
+									className={active === 'profile' ? styles.selected : styles.text}>
 									Profile
 								</a>
 							</Link>
@@ -65,8 +68,8 @@ export default function SidebarNavigation() {
 						<li>
 							<Link href='/login'>
 								<a
-									onClick={() => setActive(4)}
-									className={active === 4 ? styles.selected : styles.text}>
+									onClick={() => setActive('login')}
+									className={active === 'login' ? styles.selected : styles.text}>
 									Login
 								</a>
 							</Link>

45eac9e2b677fe50667fec102df8617d0ff0a7a3
Sean - <sean.seah0311@gmail.com>
2022-04-29 23:43:38 +0800

    update runner DB

--- docker-misc/postgres/init.sh
+++ docker-misc/postgres/init.sh
@@ -209,23 +209,23 @@ EOSQL
 #create runner_db database
 echo "Init Runner DB"
 psql -d "$RUNNER_DB" -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE TABLE challenges (
-        challenge_id varchar(64) PRIMARY KEY,
-        challenge_name varchar(255) NOT NULL,
-        docker_compose bool NOT NULL,
-        port_count int NOT NULL,
-        internal_port varchar(255),
-        image_name varchar(255),
-        docker_cmds varchar(255),
-        docker_compose_file text
-    );
-    CREATE TABLE instances (
-        instance_id SERIAL PRIMARY KEY,
-        usr_id int NOT NULL,
-        challenge_id varchar(64) NOT NULL,
-        portainer_id varchar(64) NOT NULL DEFAULT '',
-        instance_timeout bigint NOT NULL,
-        ports_used varchar(255) NOT NULL
-    );
+CREATE TABLE challenges (
+	challenge_id varchar(64) PRIMARY KEY,
+	challenge_name varchar(255) NOT NULL,
+	docker_compose bool NOT NULL,
+	port_count int NOT NULL,
+	internal_port varchar(255),
+	image_name varchar(255),
+	docker_cmds varchar(255),
+	docker_compose_file text
+);
+CREATE TABLE instances (
+	instance_id SERIAL PRIMARY KEY,
+	usr_id varchar(64) NOT NULL,
+	challenge_id varchar(64) NOT NULL,
+	portainer_id varchar(64) NOT NULL DEFAULT '',
+	instance_timeout bigint NOT NULL,
+	ports_used varchar(255) NOT NULL
+);
 EOSQL
 echo "DONE!!!"

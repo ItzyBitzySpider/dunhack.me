1af8351848fc3bb2713cee2faa45196dfa253d8f
Sean - <sean.seah0311@gmail.com>
2022-10-02 20:30:43 +0800

    resolve prisma bug

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,10 @@
 import NextAuth from 'next-auth';
-import { PrismaClient } from '@prisma/client';
+import prisma from "../../../server/databaseFunctions";
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import EmailProvider from 'next-auth/providers/email';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import DiscordProvider from 'next-auth/providers/discord';
-
-const prisma = new PrismaClient();
 
 export default NextAuth({
 	adapter: PrismaAdapter(prisma),

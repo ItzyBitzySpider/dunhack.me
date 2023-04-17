d63f9a1f3ee5c71897561e011b88ef407d327e21
sawntoe <dscsean77@gmail.com>
2023-04-17 17:00:19 +0800

    Added polling service for entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-sleep 60 #accomodate postgres DB startup without triggering shutdown
+# sleep 60 #accomodate postgres DB startup without triggering shutdown
+until psql -c 'SELECT * FROM Account'
+do
+    echo 'Waiting...'
+    sleep 2
+done
 npm run build
 npm run start
